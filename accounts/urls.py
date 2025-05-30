e51f3c39d5262e51f54fc60fcfc7ee1ef9076745
nicolep999 <nicolepavlova9@gmail.com>
2025-05-30 17:25:34 +0300

    pet model added

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,20 @@ from django.urls import path, include
 from accounts import views
 
 urlpatterns = [
-    path("register/", views.register_view),
-    path("login/", views.login_view),
-    path("profile/<int:pk>/", include([
-        path('', views.profile_details_view, name="profile-details",),
-        path("edit/", views.profile_edit_view, name="profile-edit"),
-        path('delete/', views.profile_delete_view, name='profile-delete')
-    ])),
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path(
+        "profile/<int:pk>/",
+        include(
+            [
+                path(
+                    "",
+                    views.profile_details_view,
+                    name="profile-details",
+                ),
+                path("edit/", views.profile_edit_view, name="profile-edit"),
+                path("delete/", views.profile_delete_view, name="profile-delete"),
+            ]
+        ),
+    ),
 ]
